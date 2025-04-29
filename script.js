328046ffa928779acdb5f88fc50d35ec281127c8
Abu Bakar <coolabubakar2011@gmail.com>
2025-04-29 22:40:34 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Show letter popup
+function showMessage() {
+    document.getElementById('popup').style.display = "block";
+}
+
+// Close letter popup
+function closeMessage() {
+    document.getElementById('popup').style.display = "none";
+}
